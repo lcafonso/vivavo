f556f3c00165e4302457699f7ed331d3f0e08bbb
lcafonso <lcafonso@gmail.com>
2018-10-10 15:50:22 +0100

    Atualização em 10/10/2018 v2

--- public/Avos.php
+++ public/Avos.php
@@ -1,11 +1,11 @@
 
 		<?php
 
-			
-                
-           
 
-        
+
+
+
+
 			$dsn = 'mysql:host=localhost;dbname=vivavo_data';
 			$user = 'root';
 			$password = 'esact.ipb.pt';
@@ -21,7 +21,7 @@
 
 
 			//$stmt = $conn->prepare("SELECT * FROM wp_leafletmapsmarker_markers") ;
-            
+
 
 			//$emparray = array();
 			//$myLat = floatval($_GET["lat"]);
@@ -29,68 +29,64 @@
 			//$myRng = floatval($_GET["range"]);
 
 			$nID = 0;
-		
-						
+
+
 			$rs = $conn->query("SELECT * FROM grandmas") ;
-			
+
 			if(!$rs){
      			 print_r($conn->errorInfo());
    			}
-			
-		
-			
+
+
+
 			//echo 'Latitude: ' . $myLat . '<br />';
       		//echo 'Longitude: ' . $myLng . '<br />';
       		//echo 'Distancia: ' . $myRng . '<br /><br />';
-			
-			
-			
-		
+
+
+
+
    			foreach ($rs as $row){
    				//$thisLat = floatval($row['latitude']);
 				//$thisLng = floatval($row['longitude']);
 
 				//$myDist = round(distancia($myLat,$myLng, $thisLat, $thisLng, "K"),2);
-				
+
 				//echo 'Latitude: ' . $thisLat . '<br />';
       			//echo 'Longitude: ' . $thisLng . '<br />';
       			//echo 'Distancia: ' . $myDist . '<br /><br />';
 
 				//if( floatval($myDist) < $myRng ) {
 
-      				
-      				
+
+
       				$arr =  array('id' => strval($nID+1),
 								'avo_aldeia' => $row['id'],
 								'Nome' => $row['name'],
 								'Descricao' => $row['excerpt'],
 								'Image' => $row['file'], );
-										
-				    
-					
-                
-                 
-                   
-                 
-				
-                     
-                   
-					
-      				$lista[$nID] = $arr;
-                    
+
+
+
+
+
+
+
+
+
+
+
+      				$lista[$nID] = array_map("utf8_encode",$arr);
+
       				//echo '<br />';
       				//echo $lista[$nID];
-                    
+
       				$nID++;
 
       			}
-   			
+
    			//echo 'Lista Final <br />';
 			$resposta = array('grandmas' => $lista );
 			echo json_encode($resposta);
-          
+
 		?>
-
-
-
-	
